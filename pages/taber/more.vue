e186a189d28caafc5cedd08ccacda91809387070
hq01015432 <liu15726650917@163.com>
2020-11-24 18:27:11 +0800

    修改更多

--- pages/taber/more.vue
+++ pages/taber/more.vue
@@ -10,14 +10,36 @@
 				</div>
 				<div class="more-toncent-dispeo">
 					<img src="static/homeImg/more/event.png" />
-					<span class="more-toncent-woders">我的评价</span>
+					<span class="more-toncent-woders">帮助中心</span>
 				</div>
 				<div class="more-toncent-dispeo">
 					<img src="static/homeImg/more/event.png" />
-					<span class="more-toncent-woders">我的评价</span>
+					<span class="more-toncent-woders">推荐好友</span>
 				</div>
 			</div>
-
+		</div>
+		<div class="more">
+			<p class="more-title">工人服务</p>
+			<div class="more-toncent">
+				<div class="more-toncent-dispeo">
+					<img src="static/homeImg/more/event.png" />
+					<span class="more-toncent-woders">店&nbsp;&nbsp;&nbsp;&nbsp;铺</span>
+				</div>
+				<div class="more-toncent-dispeo">
+					<img src="static/homeImg/more/event.png" />
+					<span class="more-toncent-woders">排&nbsp;&nbsp;&nbsp;&nbsp;名</span>
+				</div>
+				<div class="more-toncent-dispeo">
+					<img src="static/homeImg/more/event.png" />
+					<span class="more-toncent-woders">活&nbsp;&nbsp;&nbsp;&nbsp;动</span>
+				</div>
+			</div>
+			<div class="more-toncent" style="display: block;">
+				<div class="more-toncent-dispeo">
+					<img src="static/homeImg/more/event.png" />
+					<span class="more-toncent-woders">店&nbsp;&nbsp;&nbsp;&nbsp;铺</span>
+				</div>
+			</div>
 		</div>
 	
 	</view>
@@ -30,6 +52,7 @@
 	.contents{
 		height: 100%;
 		position: absolute;
+		width: 100%;
 	}
 	.contentsHeaImg{
 		width: 100%;
@@ -42,18 +65,36 @@
 		margin-top: 20rpx;
 	}
 	.more-toncent{
-		border-top: 1px solid #e5e5e5;
-		text-align: center;
+		    display: flex;
+		    border-top: 1px solid #e5e5e5;
+		    text-align: center;
+		    justify-content: center;
+			padding-top: 26rpx;
 	}
 	.more-toncent .more-toncent-dispeo {
 		float: left;
-		width: 200rpx;
-		height: 200rpx;
+		width: 33.33%;
+		height: 162rpx;
 		text-align: center;
-		float: left;
+		position: relative;
 	}
 	.more-toncent .more-toncent-dispeo img {
 		width: 90rpx;
 		height: 90rpx;
 	}
+	.more-toncent-woders {
+		float: left;
+		margin-left: 62rpx;
+		color: #757575;
+		width: 56px;
+	}
+	.more-toncent-dispeo:nth-of-type(1):before, .more-toncent-dispeo:nth-of-type(2):before {
+        content: "";
+        position: absolute;
+        right: 6px;
+        top: 9px;
+        width: 1%;
+        height: 44px;
+        background-color: #e5e5e5;
+    }
 </style>
